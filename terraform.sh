57af99b7348346cd2099fe4e7f352532637b23b6
James Gonzalez <james.gonzalez@contino.io>
2019-08-01 12:09:55 +0100

    Adding lots of new options and tweaks for CI pipelines, no color, automation mode, etc

--- terraform.sh
+++ terraform.sh
@@ -1,64 +1,76 @@
 #!/bin/bash
-# Author: James Gonzalez - This is a wrapper for Terraform
+
+set -e
 
 show_help() {
   cat >/dev/stdout <<END
-$0 -b <bucket_name> -r <primary_region> -k <tf_state_s3_path> -R <region_of_infra> [ -d true # to destroy | -i true # to init only]
+$0 -b <bucket_name> -r <primary_region> -k <tf_state_s3_path> -R <region_of_infra> -I <extra_init_options> -P <extra_plan_options> [ -d true # to destroy | -i true # to init only | -D true # for debugging mode]
   Example:
-  $0 -b my-cool-bucket -r eu-west-1 -k eu-west-1/dev/sevice/terraform.tfstate -R eu-west-2
-  $0 -b my-cool-bucket -r eu-west-1 -k eu-west-1/dev/sevice/terraform.tfstate -R eu-west-2 -d true # DESTROY
-  $0 -b my-cool-bucket -r eu-west-1 -k eu-west-1/dev/sevice/terraform.tfstate -R eu-west-2 -i true # INIT ONLY (debugging, allows you to run your normal terraform commands afterwards on local machines)
+  $0 -b my-cool-bucket -r eu-west-1 -k eu-west-1/dev/sevice/terraform.tfstate -I "-plugin-dir=/providers" -I no-color -P "-var-file=dev-env.tfvars"
+  $0 -b my-cool-bucket -r eu-west-1 -k eu-west-1/dev/sevice/terraform.tfstate -d true # DESTROY
+  $0 -b my-cool-bucket -r eu-west-1 -k eu-west-1/dev/sevice/terraform.tfstate -i true # INIT ONLY (debugging, allows you to run your normal terraform commands afterwards on local machines)
 END
-exit 0
+  exit 0
 }
 
 ## Defaults
-BUCKET_KEY="terraform.tfstate" ## Example of how you can have a structure to your tfstate files
-STATE_BUCKET_NAME="my-tfstate-bucket"
+BUCKET_KEY="" ## Example of how you can have a structure to your tfstate files
+STATE_BUCKET_NAME=""
 STATE_BUCKET_REGION="us-east-2"
-REGION="us-east-2"
+TF_IN_AUTOMATION="1"
 
-while getopts ":hb:k:d:i:r:R:" opt; do
+export TF_IN_AUTOMATION
+
+while getopts ":hb:k:d:i:r:I:P:D:" opt; do
   case ${opt} in
     b) STATE_BUCKET_NAME=${OPTARG} ;;
     k) BUCKET_KEY=${OPTARG} ;;
     r) STATE_BUCKET_REGION=${OPTARG} ;;
-    R) REGION=${OPTARG} ;;
     i) INIT_ONLY=${OPTARG} ;;
     d) DESTROY=${OPTARG} ;;
-    h | \? ) show_help
+    D) DEBUGGING=${OPTARG} ;;
+    I) EXTRA_INIT+=("$OPTARG") ;;
+    P) EXTRA_PLAN+=("$OPTARG") ;;
+    h | \?)
+      show_help
       ;;
   esac
 done
+shift $((OPTIND - 1))
 
 init() {
   echo "---- Launching Terraform init ---"
   terraform init -backend-config="key=${BUCKET_KEY}" \
     -backend-config="bucket=${STATE_BUCKET_NAME}" \
     -backend-config="region=${STATE_BUCKET_REGION}" \
-    -backend=true -force-copy -get=true -input=false -reconfigure
+    -backend=true -force-copy -get=true -input=false -reconfigure "${EXTRA_INIT[@]}"
 }
 
 plan() {
   set -e
   echo "---- Launching Terraform plan ---"
-  terraform plan -refresh=true -lock=true \
-    -var "region=${REGION}" \
-    -out terraform.plan
+  terraform plan -refresh=true -lock=true -no-color \
+    -out terraform.plan "${EXTRA_PLAN[@]}"
 }
 
 apply() {
   set -e
   echo "---- Launching Terraform apply ---"
-  terraform apply -lock=true terraform.plan
+  terraform apply -no-color -lock=true terraform.plan
 }
-
 
 destroy() {
   set -e
-  echo "---- Launching Terraform apply ---"
-  terraform apply -lock=true terraform.plan
+  echo "---- Launching Terraform destroy ---"
+  terraform destroy -no-color -lock=true terraform.plan
 }
+
+if [[ ${DEBUGGING} == "true" ]]; then
+  export TF_LOG="DEBUG"
+  echo "Terraform options: Running with TF_LOG set to: ${TF_LOG}"
+fi
+
+echo "Terraform options: Running with TF_IN_AUTOMATION set to : ${TF_IN_AUTOMATION}"
 
 if [[ ${INIT_ONLY} == "true" ]]; then
   echo " ## ONLY RUNNING TF INIT FOR LOCAL DEBUGGING ##"
@@ -69,8 +81,7 @@ elif [[ ${DESTROY} == "true" ]]; then
 else
   init
   plan
-  # apply
+  apply
 fi
-
 
 exit 0
